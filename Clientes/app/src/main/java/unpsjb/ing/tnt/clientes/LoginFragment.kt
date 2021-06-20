083170dbcd0cdf4b5970722961cde010d3e9f679
bianchinig <bianchiniger@gmail.com>
2021-06-20 15:37:08 -0300

    Agregada navegación inicial y principio de listado de tiendas

--- Clientes/app/src/main/java/unpsjb/ing/tnt/clientes/LoginFragment.kt
+++ Clientes/app/src/main/java/unpsjb/ing/tnt/clientes/LoginFragment.kt
@@ -34,7 +34,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     companion object {
--- Vendedores/app/src/main/java/unpsjb/ing/tnt/vendedores/data/model/Pedido.kt
+++ Vendedores/app/src/main/java/unpsjb/ing/tnt/vendedores/data/model/Pedido.kt
@@ -2,6 +2,8 @@ package unpsjb.ing.tnt.vendedores.data.model
 
 import com.google.firebase.Timestamp
 
+val STATES = arrayOf<String>("creado", "en-curso", "preparado", "entregado")
+
 class Pedido(val id: String, val productos: ArrayList<String>, val estado: String, val estampaDeTiempo: Timestamp) {
     fun getFormattedId(): String {
         return "Pedido #$id"
